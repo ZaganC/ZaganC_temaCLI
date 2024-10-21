f9d132e0b358bc588e7589f501c3deccbc878883
Zagan Cristian <cristian.zagan@student.usv.ro>
2024-10-21 17:40:49 +0300

    Add project files.

---
+++ Lab02/Lab02/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace OpenTK_console_sample01
+{
+    class SimpleWindow : GameWindow
+    {
+        private float objectPosX = 0.0f;
+        private float objectPosY = 0.0f; 
+
+
+        public SimpleWindow() : base(800, 600)
+        {
+            KeyDown += Keyboard_KeyDown;
+            MouseMove += Mouse_Move;
+        }
+
+
+        void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.Exit();
+
+            if (e.Key == Key.F11)
+            {
+                if (this.WindowState == WindowState.Fullscreen)
+                    this.WindowState = WindowState.Normal;
+                else
+                    this.WindowState = WindowState.Fullscreen;
+            }
+
+
+            if (e.Key == Key.A)
+                objectPosX -= 0.05f;
+
+
+            if (e.Key == Key.D)
+                objectPosX += 0.05f;
+        }
+
+
+        void Mouse_Move(object sender, MouseMoveEventArgs e)
+        {
+
+            objectPosY = (e.Y - Height / 2f) / (Height / 2f);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(Color.MidnightBlue);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Begin(PrimitiveType.Triangles);
+
+            GL.Color3(Color.MidnightBlue);
+            GL.Vertex2(objectPosX - 1.0f, objectPosY + 1.0f); 
+            GL.Color3(Color.SpringGreen);
+            GL.Vertex2(objectPosX, objectPosY - 1.0f);
+            GL.Color3(Color.Ivory);
+            GL.Vertex2(objectPosX + 1.0f, objectPosY + 1.0f);
+
+            GL.End();
+
+            this.SwapBuffers();
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (SimpleWindow example = new SimpleWindow())
+            {
+                example.Run(30.0, 0.0);
+            }
+        }
+    }
+}
---
+++ ZaganC_temaCLI.md
@@ -0,0 +1,14 @@
+# Lab 2 - EGC
+
+**Zagan Cristian**  
+Grupa: 3133A
+
+---
+
+1. **Un viewport** este partea vizibilă a utilizatorului.
+2. **Frames per second** este frecvența la care apar pe ecran cadrele.
+3. Metoda `OnUpdateFrame()` este chemată atunci când un cadru este modificat.
+4. **Modul imediat de randare** (sau *Immediate Mode Rendering*) este un stil de programare grafică în care comenzile grafice sunt trimise direct la GPU (procesorul grafic) pentru a fi executate imediat.
+5. Ultima versiune de **OpenGL** care acceptă modul imediat este: standard **4.6**.
+6. Metoda `OnRenderFrame()` este apelată atunci când un cadru este randat.
+7. Parametrii metodei `CreatePerspectiveFieldOfView()` reprezintă **fovy**, **aspect**, **zNear** și **zFar**. Ei reprezintă proiecția perspectivei asupra matricei.
